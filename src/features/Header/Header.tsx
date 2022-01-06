7463a88879ff22cd9ca4e14da48ecb2ddb421aff
iferraro <afronerd01@outlook.com>
2022-01-05 22:56:04 -0800

    Structure for transitioned divs solved

--- src/features/Header/Header.tsx
+++ src/features/Header/Header.tsx
@@ -7,32 +7,32 @@ const Header = () => {
   const [isShowing, setIsShowing] = useState(false);
   setTimeout(() => {
     setIsShowing(true);
-  }, 500);
+  }, 150);
 
-  return isShowing ? (
-    <Transition
-      as="div"
-      show={isShowing}
-      enter="transition-opacity duration-500 ease-in-out "
-      enterFrom="opacity-0"
-      enterTo="opacity-100"
-    >
-      <div className="flex justify-center">
-        <div className="flex justify-end items-center mr-4">
-          <IsaacLogo style={{ height: 152, width: 120, fill: "#1C292C" }} />
+  return (
+    <div style={{height: 152, backgroundColor: "#EDEFEE"}}>
+      <Transition
+        as="div"
+        show={isShowing}
+        enter="transition-opacity duration-500 in-expo "
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+      >
+        <div className="flex justify-center">
+          <div className="flex justify-end items-center mr-4">
+            <IsaacLogo style={{ height: 152, width: 120, fill: "#1C292C" }} />
+          </div>
+          <div className="flex flex-col justify-center text-theme-dark">
+            <h1 className="text-6xl font-bold">
+              Isaac
+              <br />
+              Ferraro
+            </h1>
+            <h2 className="text-2xl font-bold">Web Developer</h2>
+          </div>
         </div>
-        <div className="flex flex-col justify-center text-theme-dark">
-          <h1 className="text-6xl font-bold">
-            Isaac
-            <br />
-            Ferraro
-          </h1>
-          <h2 className="text-2xl font-bold">Web Developer</h2>
-        </div>
-      </div>
-    </Transition>
-  ) : (
-    <div style={{ height: 152, backgroundColor: "#EDEFEE" }} />
+      </Transition>
+    </div>
   );
 };
 
