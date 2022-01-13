e75bd82500a6fa3e137120574dee7154eeb8c7cf
iferraro <afronerd01@outlook.com>
2022-01-12 22:03:49 -0800

    Made custom favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import logo from "./logo.svg";
 import Graph from "./features/indicator/Indicator";
 import Header from "./features/Header/Header";
 import About from "./features/About/About";
 import Projects from "./features/Projects/Projects";
 import Contact from "./features/Contact/Contact";
 import Footer from "./features/Footer/Footer";
-import Northwest from "./features/Northwest/Northwest";
 import { x } from "./features/constant";
 const App = () => {
   console.log(x, "<= this is x");
