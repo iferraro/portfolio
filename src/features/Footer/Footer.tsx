e733eae6bb16011965e416a05ed80c9610072bc0
iferraro <afronerd01@outlook.com>
2022-01-13 23:02:25 -0800

    Added mention of Tailwindcss in Footer

--- src/features/Footer/Footer.tsx
+++ src/features/Footer/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className="text-center">
       <br />
       <p className="font-bold">
-        This site was made using TypeScript, React and Redux, and deployed via Netlify
+        This site was made using TypeScript, React, Redux, and Tailwindcss; deployed via Netlify
       </p>
       <br />
     </div>
