d09b9ba013c146e4bc1479cc9ecd75035b0d5037
iferraro <afronerd01@outlook.com>
2022-02-22 22:19:41 -0800

    Added pictures to Projects

--- src/features/Projects/Projects.tsx
+++ src/features/Projects/Projects.tsx
@@ -1,99 +1,20 @@
 import React from "react";
 import { x } from "../constant";
-
-interface Project {
-  // iconLink: string;
-  title: string;
-  description: string;
-  stack: string[];
-  siteLink: string;
-  gitHubLink: string;
-}
-
-class CompletedProject {
-  // iconLink: string;
-  title: string;
-  description: string;
-  stack: string[];
-  siteLink: string;
-  gitHubLink: string;
-
-  constructor(
-    // iconLink: string,
-    title: string,
-    description: string,
-    stack: string[],
-    siteLink: string,
-    gitHubLink: string
-  ) {
-    // this.iconLink = iconLink;
-    this.title = title;
-    this.description = description;
-    this.stack = stack;
-    this.siteLink = siteLink;
-    this.gitHubLink = gitHubLink;
-  }
-}
-
-const mancala: Project = new CompletedProject(
-  "Mancala",
-  "A recreation of the classic board game Mancala",
-  ["Vanilla JavaScript", "HTML", "CSS"],
-  "https://iferraro.github.io/mancala/",
-  "https://github.com/iferraro/mancala"
-);
-
-const blogOfTheBeetle: Project = new CompletedProject(
-  "Blog of the Beetle",
-  "A beetle image upload site",
-  ["Node", "Express", "MongoDB", "Passport", "Google API"],
-  "https://blog-of-the-beetle.herokuapp.com/home",
-  "https://github.com/iferraro/blog-of-the-beetle"
-);
-
-const bightOfBytes: Project = new CompletedProject(
-  "Bight of Bytes",
-  "A smartphone info tool",
-  ["Python", "Django", "PostgreSQL"],
-  "https://bight-of-bytes.herokuapp.com/",
-  "https://github.com/iferraro/bight-of-bytes"
-);
-
-const theMernBible: Project = new CompletedProject(
-  "The MERN Bible",
-  "A King James Version Bible reference",
-  ["MongoDB", "Express", "React", "Node", "Tailwind CSS", "Headless UI"],
-  "https://mern-bible.herokuapp.com/",
-  "https://github.com/iferraro/mern-bible"
-);
-
-const customColorChart: Project = new CompletedProject(
-  "Custom Color Chart",
-  "My personal favorite colors, named by me",
-  ["Vanilla JavaScript", "jQuery", "HTML", "CSS"],
-  "https://iferraro.github.io/custom-color-chart/",
-  "https://github.com/iferraro/custom-color-chart"
-);
-
-const completedProjects = [
-  theMernBible,
-  bightOfBytes,
-  blogOfTheBeetle,
-  mancala,
-  customColorChart,
-];
+import { projects } from "./projects";
 
 const makeItems = (
   arr: Array<{
     title: string;
     description: string;
     stack: string[];
+    imageLink: string;
     siteLink: string;
     gitHubLink: string;
   }>
 ) => {
   return arr.map((item, i) => (
-    <div key={i} style={{ width: x }} className="mx-6">
+    <div key={i} style={{ width: x }} className="m-6">
+      <img src={item.imageLink} alt={item.title} />
       <h1 className="text-2xl font-bold">{item.title}</h1>
       <h2>{item.description} using:</h2>
       <ul className="list-disc">
@@ -115,7 +36,7 @@ const makeItems = (
   ));
 };
 
-const projectItems = makeItems(completedProjects);
+const projectItems = makeItems(projects);
 
 const Projects = () => {
   return (
@@ -126,7 +47,10 @@ const Projects = () => {
         Projects
       </h1>
       <br />
-      <div className="flex flex-wrap justify-center mx-auto" style={{ maxWidth: 2 * x + 96}}>
+      <div
+        className="flex flex-wrap justify-center mx-auto"
+        style={{ maxWidth: 2 * x + 96 }}
+      >
         {projectItems}
       </div>
     </div>
---
+++ src/features/Projects/projects.ts
@@ -0,0 +1,79 @@
+class Project {
+  title: string;
+  description: string;
+  stack: string[];
+  imageLink: string;
+  siteLink: string;
+  gitHubLink: string;
+
+  constructor(
+    title: string,
+    description: string,
+    stack: string[],
+    imageLink: string,
+    siteLink: string,
+    gitHubLink: string
+  ) {
+    this.title = title;
+    this.description = description;
+    this.stack = stack;
+    this.imageLink = imageLink;
+    this.siteLink = siteLink;
+    this.gitHubLink = gitHubLink;
+  }
+}
+
+const mancala: Project = new Project(
+  "Mancala",
+  "A recreation of the classic board game Mancala",
+  ["Vanilla JavaScript", "HTML", "CSS"],
+  "https://i.imgur.com/HhfyApD.png",
+  "https://iferraro.github.io/mancala/",
+  "https://github.com/iferraro/mancala"
+);
+
+const blogOfTheBeetle: Project = new Project(
+  "Blog of the Beetle",
+  "A beetle image upload site",
+  ["Node", "Express", "MongoDB", "Passport", "Google API"],
+  "https://i.imgur.com/JMkEoPK.png",
+  "https://blog-of-the-beetle.herokuapp.com/home",
+  "https://github.com/iferraro/blog-of-the-beetle"
+);
+
+const bightOfBytes: Project = new Project(
+  "Bight of Bytes",
+  "A smartphone info tool",
+  ["Python", "Django", "PostgreSQL"],
+  "https://i.imgur.com/fINTRrP.png",
+  "https://bight-of-bytes.herokuapp.com/",
+  "https://github.com/iferraro/bight-of-bytes"
+);
+
+const theMernBible: Project = new Project(
+  "The MERN Bible",
+  "A King James Version Bible reference",
+  ["MongoDB", "Express", "React", "Node", "Tailwind CSS", "Headless UI"],
+  "https://i.imgur.com/TLXCAtY.png",
+  "https://mern-bible.herokuapp.com/",
+  "https://github.com/iferraro/mern-bible"
+);
+
+const customColorChart: Project = new Project(
+  "Custom Color Chart",
+  "My personal favorite colors, named by me",
+  ["Vanilla JavaScript", "jQuery", "HTML", "CSS"],
+  "https://i.imgur.com/SUK7MkJ.png",
+  "https://iferraro.github.io/custom-color-chart/",
+  "https://github.com/iferraro/custom-color-chart"
+);
+
+const projects = [
+  theMernBible,
+  bightOfBytes,
+  blogOfTheBeetle,
+  mancala,
+  customColorChart,
+];
+
+export { projects };
